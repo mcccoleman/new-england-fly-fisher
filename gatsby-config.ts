3462876d201eaecaa23684e72cf0c91347cf5114
Coleman <coleman@Coleman.local>
2023-04-20 14:38:31 -0400

    add header component

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,23 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `new-england-fly-fisher`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.newenglandflyfisher.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-root-import",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/components/header/logo.png",
+      },
+    },
+  ],
+};
 
-export default config
+export default config;
