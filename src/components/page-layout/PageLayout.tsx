5a6a3ec9fd8af4261c5eb68586306881b06f1b81
Michael Coleman <37200650+mcccoleman@users.noreply.github.com>
2023-04-23 11:02:58 -0400

    fix image resize issue (#5)

--- src/components/page-layout/PageLayout.tsx
+++ src/components/page-layout/PageLayout.tsx
@@ -4,6 +4,14 @@ import "./pageLayout.css";
 import { Header } from "../header";
 import styled from "styled-components";
 import { Flex } from "src/shared/components/Flex";
+import { graphql } from "gatsby";
+import { PageLayoutFragment } from "graphql-types";
+
+export const pageLayout = graphql`
+  fragment PageLayout on Query {
+    ...Header
+  }
+`;
 
 const PageLayoutWrapper = styled(Flex)`
   padding: 10px 150px;
@@ -15,11 +23,13 @@ const PageLayoutWrapper = styled(Flex)`
   background-color: #6e8fc5;
 `;
 
-interface LayoutProps extends PropsWithChildren {}
+interface LayoutProps extends PropsWithChildren {
+  data: PageLayoutFragment;
+}
 
-export const PageLayout: FC<LayoutProps> = ({ children }) => (
+export const PageLayout: FC<LayoutProps> = ({ data, children }) => (
   <PageLayoutWrapper column>
-    <Header />
+    <Header data={data} />
     {children}
   </PageLayoutWrapper>
 );
