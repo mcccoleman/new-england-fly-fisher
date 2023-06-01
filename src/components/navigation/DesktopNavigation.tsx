38fa54eeea9e2786f349d1eb8442ea00cfada22d
mcccoleman <colemanmc02@gmail.com>
2023-05-31 21:31:42 -0400

    add underline for current page

--- src/components/navigation/DesktopNavigation.tsx
+++ src/components/navigation/DesktopNavigation.tsx
@@ -4,10 +4,20 @@ import { Flex } from "../flex";
 import styled from "styled-components";
 import { H4 } from "../typography";
 import { StyledLink } from "./shared";
+import { useLocation } from "@reach/router";
 
-const StyledH4 = styled(H4)`
+interface StyledH4Props {
+  isCurrentPath: boolean;
+}
+
+const StyledH4 = styled(H4)<StyledH4Props>`
   color: white;
   cursor: pointer;
+  text-decoration: ${({ isCurrentPath }) =>
+    isCurrentPath ? "underline" : "unset"};
+
+  text-decoration-thickness: 3px;
+  text-underline-offset: 8px;
 `;
 
 const DesktopNavigationWrapper = styled(Flex)`
@@ -19,12 +29,18 @@ const DesktopNavigationWrapper = styled(Flex)`
 
 interface DesktopNavigationProps {}
 
-export const DesktopNavigation: FC<DesktopNavigationProps> = () => (
-  <DesktopNavigationWrapper alignItems="end" justifyContent="center">
-    {NAVIGATION_LINKS.map(({ link, title }) => (
-      <StyledLink to={link} key={title}>
-        <StyledH4>{title}</StyledH4>
-      </StyledLink>
-    ))}
-  </DesktopNavigationWrapper>
-);
+export const DesktopNavigation: FC<DesktopNavigationProps> = () => {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <DesktopNavigationWrapper alignItems="end" justifyContent="center">
+      {NAVIGATION_LINKS.map(({ link, title }) => (
+        <StyledLink to={link} key={title}>
+          <StyledH4 isCurrentPath={location.pathname.includes(link)}>
+            {title}
+          </StyledH4>
+        </StyledLink>
+      ))}
+    </DesktopNavigationWrapper>
+  );
+};
